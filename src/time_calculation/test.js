6a2f380b0ab07818161768e00ae6afe03db7dc22
Konstantin Dobler <konstantin.j.dobler@gmail.com>
2019-11-01 00:01:19 +0000

    first commit

---
+++ src/time_calculation/test.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Form from "react-bootstrap/Form"
+class Test extends React.Component {
+    render() {
+        return (
+            <div>Hello World</div>
+        )
+    }
+}
